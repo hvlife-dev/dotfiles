2c123fcbeb526d7b56d4c5e2aab5a9e6236c2d22
HVLife <hvismylife@protonmail.com>
2025-05-16 18:13:32 +0200

    rolling update

--- .local/bin/smbHassos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p /run/media/hvlife/win
-mount /dev/nvme0n1p2 /run/media/hvlife/win
-
-mount --mkdir -t cifs //192.168.1.10/media /run/media/hvlife/hassos/DB -o username=hvlife,password="/\/\'"
-mount --mkdir -t cifs //192.168.1.10/Data /run/media/hvlife/hassos/Vid -o username=hvlife,password="/\/\'"
-
